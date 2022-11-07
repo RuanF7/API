52c9816e1c03b2541d6425b26b4d76f2a4279cb5
Ruan Finamor <75641084+RuanF7@users.noreply.github.com>
2022-11-07 18:20:09 -0300

    Adicionando pasta php

---
+++ php/listar.php
@@ -0,0 +1,30 @@
+<?php
+
+//Incluir a conexao
+include("conexao.php");
+
+//Sql
+$sql = "SELECT * FROM cursos";
+
+//Executar
+$executar = mysqli_query($conexao, $sql);
+
+//Vetor
+$cursos = [];
+
+//Indice
+$indice = 0;
+
+//Laço
+while($linha = mysqli_fetch_assoc($executar)){
+    $cursos[$indice]['idCurso'] = $linha['idCurso'];
+    $cursos[$indice]['nomeCurso'] = $linha['nomeCurso'];
+    $cursos[$indice]['valorCurso'] = $linha['valorCurso'];
+
+    $indice++;
+}
+
+//JSON
+json_encode(['cursos'=>$cursos]);
+
+?>
